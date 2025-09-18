c0546c5ff0bd9cdfc8457bd928ee7e42cf86a14c
Nolverto Urias Obeso <nolvertou@gmail.com>
2025-09-17 21:55:26 -0700

    Create E009_change_associated_action_of_macros_and_use_quit_count.sv

---
+++ Section2: Reporting_Mechanism/E009_change_associated_action_of_macros_and_use_quit_count.sv
@@ -0,0 +1,43 @@
+// File: 		E009_change_associated_action_of_macros_and_use_quit_count.sv
+// Description: Changing assocuated actions of macros and use of quit_count to limit number of UVM_ERROR and finish the test
+
+
+`include "uvm_macros.svh"
+import uvm_pkg::*;
+ 
+//////////////////////////////////////////////////
+class driver extends uvm_driver;
+  `uvm_component_utils(driver)
+  
+  function new(string path , uvm_component parent);
+    super.new(path, parent);
+  endfunction
+ 
+  task run();
+    `uvm_info("DRV", "Informational Message", UVM_NONE);
+    `uvm_warning("DRV", "Potential Error");
+    `uvm_error("DRV", "Real Error"); ///uvm_count
+    `uvm_error("DRV", "Second Real Error");
+    /*
+     #10;
+    `uvm_fatal("DRV", "Simulation cannot continue DRV1"); /// uvm_exit
+    #10;
+    `uvm_fatal("DRV1", "Simulation Cannot Continue DRV1");
+   */
+  endtask
+  
+endclass
+ 
+/////////////////////////////////////////////
+ 
+ 
+module tb;
+  driver d;
+  
+  initial begin
+    d = new("DRV", null);
+    d.set_report_max_quit_count(3);
+    d.run();
+  end
+  
+endmodule
